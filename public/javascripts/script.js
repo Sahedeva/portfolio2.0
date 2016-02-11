f412e0d194a8d828ac3b4bb196f6f2a008e9906b
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-02-11 16:02:30 -0600

    added consoleCat

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,26 @@
+console.log("             *     ,MMM8&&&.            *");
+console.log("                  MMMM88&&&&&    .");
+console.log("                 MMMM88&&&&&&&");
+console.log("     *           MMM88&&&&&&&&");
+console.log("                 MMM88&&&&&&&&");
+console.log("                 'MMM88&&&&&&'");
+console.log("                   'MMM8&&&'      *");
+console.log("          |\\___/|");
+console.log("          )     (             .              '");
+console.log("         =\\     /=");
+console.log("           )===(       *");
+console.log("          /     \\");
+console.log("          |     |");
+console.log("         /       \\");
+console.log("         \\       /");
+console.log("  _/\\_/\\_/\\__  _/_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_");
+console.log("  |  |  |  |( (  |  |  |  |  |  |  |  |  |  |");
+console.log("  |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |");
+console.log("  |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |");
+console.log("  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |");
+console.log("  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |");
+console.log(" ");
+console.log("         consoleCat - by Bob   (MEOW)");
 // create the module and name it portfolioApp
 var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
 
